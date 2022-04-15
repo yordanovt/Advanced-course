6187376844388147e802aa3723e6e3bb7f8265a1
yorda <yorda@DESKTOP-VI2A4HV>
2022-04-15 22:04:27 +0300

    Add project files.

---
+++ Advanced course/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_course
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input  = Console.ReadLine();
+            var demo = new Stack<char>();
+           
+            foreach (var item in input)
+            {
+                demo.Push(item);
+                
+            }
+            while (demo.Count!=0)
+            {
+                Console.Write(demo.Pop());
+            }
+            Console.WriteLine();
+            
+        }
+    }
+}
