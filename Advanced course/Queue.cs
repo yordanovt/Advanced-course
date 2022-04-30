38d75e99e1832b961c912a5a06203f201c977a6d
yordanovt <yordanov.toni@gmail.com>
2022-04-30 16:54:27 +0300

    Adding classes

---
+++ Advanced course/Queue.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_course
+{
+    internal class Queue
+    {
+        public static void methodQueue(string input)
+        {
+            var demoQueue = new Queue<char>();
+
+            foreach (var item in input)
+            {
+                demoQueue.Enqueue(item);
+                Console.WriteLine(item);
+            }
+            while (demoQueue.Count != 0)
+            {
+                Console.Write(demoQueue.Dequeue());
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Advanced course/Stack.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advanced_course
+{
+    internal class Stack
+    {
+        public static void methodStack(string input)
+        {
+            var demoStack = new Stack<char>();
+
+            foreach (var item in input)
+            {
+                demoStack.Push(item);
+                Console.WriteLine(item);
+            }
+            while (demoStack.Count != 0)
+            {
+                Console.Write(demoStack.Pop());
+            }
+            Console.WriteLine();
+
+            
+        }
+    }
+}
