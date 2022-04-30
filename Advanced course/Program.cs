9e8e911f976382f2c63b5ab5be8775e9c06b10ce
yordanovt <yordanov.toni@gmail.com>
2022-04-30 12:17:34 +0300

    adding Queue and Stack example

--- Advanced course/Program.cs
+++ Advanced course/Program.cs
@@ -12,20 +12,32 @@ namespace Advanced_course
         static void Main(string[] args)
         {
             var input  = Console.ReadLine();
-            var demo = new Stack<char>();
-           
+            var demoStack = new Stack<char>();
+            var demoQueue = new Queue<char>();
+
             foreach (var item in input)
             {
-                demo.Push(item);
+                demoStack.Push(item);
                 Console.WriteLine(item);
             }
-            
-            while (demo.Count!=0)
+            while (demoStack.Count != 0)
             {
-                Console.Write(demo.Pop());
+                Console.Write(demoStack.Pop());
             }
-            Console.WriteLine("this is a change to add in github for testing purpose");
-            
+            Console.WriteLine();
+            foreach (var item in input)
+            {
+                demoQueue.Enqueue(item);
+                Console.WriteLine(item);
+            }
+
+            while (demoQueue.Count != 0)
+            {
+                Console.Write(demoQueue.Dequeue());
+            }
+            Console.WriteLine();
+
+
         }
     }
 }
